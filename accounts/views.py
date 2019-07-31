c5483e678cba11664fbb1e2719658a8543991653
sunshane6726 <sunshane6726@gmail.com>
2019-08-01 00:46:21 +0900

    views.py_username

--- accounts/views.py
+++ accounts/views.py
@@ -17,7 +17,7 @@ def signup(request):
         # User has info and wants an account now!
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.objects.get(username=request.POST['email'])
+                user = User.objects.get(username=request.POST['username'])
                 return render(request, 'accounts/signup.html', {'error': 'Username has already been taken'})
             except User.DoesNotExist:
                 user = User.objects.create_user(
